e33bdb14af18a571e17d647d75ec6ce6e5c95494
Yastrebov.A.S <70572775+YastrebovAS@users.noreply.github.com>
2021-05-25 11:56:59 +0300

    Add files via upload

---
+++ headers.h
@@ -0,0 +1,15 @@
+#ifndef HEADERS_H
+#define HEADERS_H
+#ifndef SCNu8
+#define SCNu8 "hhu"
+#endif
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include <inttypes.h>
+#include<string.h>
+#include"Polynomial.h"
+#include"Func.h"
+#include"PolynomialInt.h"
+#include"PolynomialDouble.h"
+#endif //Headers
